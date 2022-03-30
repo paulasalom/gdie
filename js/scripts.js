3f81d67a6b9cb6c40c66f78bfce22d03faf3641d
juanjopuya <juanjopuya@gmail.com>
2022-03-30 22:55:26 +0200

    add mapa primera version

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,17 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+
+
+
 });
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
+
