783192200a1fe2a45ff084df0cf80fd0215ee7ca
paulasalom <paulaferrer2000@hotmail.es>
2022-04-03 20:21:51 +0200

    galeria

--- js/scripts.js
+++ js/scripts.js
@@ -81,6 +81,7 @@ textTrack.oncuechange = function () {
     console.log(obj.descripcion[lang]);
     //document.getElementById('description').innerHTML = '';
     document.getElementById('description').innerHTML = '<h2 class="text-white mb-4">' + obj.titulo +'</h2> <p class="text-white-50" >'+  obj.descripcion[lang] +'</p>';
+    document.getElementById('galeria').innerHTML = '<div class="col"><img src="'+ obj.img[0]+'" class="imagen"></div><div class="col"><img src="'+ obj.img[1]+'" class="imagen"></div><div class="col"><img src="'+ obj.img[2]+'" class="imagen"></div>'
     marker = new google.maps.Marker({
         position: {lat: parseInt(obj.lat), lng: parseInt(obj.lng)},
         map,
